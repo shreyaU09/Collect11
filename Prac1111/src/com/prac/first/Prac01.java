c73a8a6800e12efca1b7749c8b376f0ac85432da
shreyaU09 <ugavekarshreya@yahoo.in>
2019-03-11 19:18:37 +0530

    First commit in this revert......

---
+++ Prac1111/src/com/prac/first/Prac01.java
@@ -0,0 +1,11 @@
+package com.prac.first;
+
+public class Prac01 {
+public static void main(String[] args) {
+	System.out.println("Helooo");
+	int a=10;
+	int b=20;
+	int c=a+b;
+	System.out.println(c);
+}
+}
---
+++ Prac1111/src/com/prac/first/Prac02.java
@@ -0,0 +1,10 @@
+package com.prac.first;
+
+public class Prac02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a="Helooo Banglore";
+	}
+
+}
