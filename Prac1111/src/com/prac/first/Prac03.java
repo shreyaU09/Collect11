409005041e9d7ab5010b84cc6b42373640ee6b02
shreyaU09 <ugavekarshreya@yahoo.in>
2019-03-11 19:25:09 +0530

    Second one commit for revert prac

---
+++ Prac1111/src/com/prac/first/Prac03.java
@@ -0,0 +1,17 @@
+package com.prac.first;
+
+public class Prac03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=90;
+		int b=70;
+		System.out.println("for the revert ");
+		System.out.println("--------------------");
+		System.out.println("we will check for update");
+		
+		
+	}
+
+}
